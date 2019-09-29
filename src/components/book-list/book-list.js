de4d41ef367c3d7271927c1204794054958d0313
vlad3k <vlad3k@bk.ru>
2019-09-29 18:06:01 +0500

    components-containers

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -10,7 +10,22 @@ import ErrorIndicator from '../error-indicator';
 
 import './book-list.css';
 
-class BookList extends Component {
+
+const BookList = ({ books }) => {
+  return (
+    <ul className="book-list">
+      {
+        books.map((book) => {
+          return (
+            <li key={book.id}><BookListItem book={book}/></li>
+          );
+        })
+      }
+    </ul>
+  );
+};
+
+class BookListContainer extends Component {
 
   componentDidMount() {
     this.props.fetchBooks();
@@ -28,19 +43,9 @@ class BookList extends Component {
       return <ErrorIndicator />;
     }
 
-    return (
-      <ul className="book-list">
-        {
-          books.map((book) => {
-            return (
-              <li key={book.id}><BookListItem book={book}/></li>
-            )
-          })
-        }
-      </ul>
-    );
+    return <BookList books={books} />
   }
-}
+};
 
 const mapStateToProps = ({ books, loading, error }) => {
   return { books, loading, error };
@@ -55,4 +60,4 @@ const mapDispatchToProps = (dispatch, { bookstoreService }) => {
 export default compose(
   withBookstoreService(),
   connect(mapStateToProps, mapDispatchToProps)
-)(BookList);
+)(BookListContainer);
