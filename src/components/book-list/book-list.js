3c51c1ea663c04cf1389a390c10ad0f03717f764
vlad3k <vlad3k@bk.ru>
2019-09-29 17:52:43 +0500

    conventional naming and refactoring

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -3,24 +3,31 @@ import BookListItem from '../book-list-item';
 import { connect } from 'react-redux';
 
 import { withBookstoreService } from '../hoc';
-import { booksLoaded } from '../../actions';
+import { fetchBooks } from '../../actions';
 import { compose } from '../../utils';
+import Spinner from '../spinner';
+import ErrorIndicator from '../error-indicator';
 
 import './book-list.css';
 
 class BookList extends Component {
 
   componentDidMount() {
-    // 1. receive data
-    const { bookstoreService } = this.props;
-    const data = bookstoreService.getBooks();
-
-    // 2. dispacth action to store
-    this.props.booksLoaded(data);
+    this.props.fetchBooks();
   }
 
   render() {
-    const { books } = this.props;
+    const { books, loading, error } = this.props;
+
+    if (loading) {
+      return <Spinner />;
+    }
+
+    if (error) {
+      console.error(error.message);
+      return <ErrorIndicator />;
+    }
+
     return (
       <ul className="book-list">
         {
@@ -35,12 +42,14 @@ class BookList extends Component {
   }
 }
 
-const mapStateToProps = ({ books }) => {
-  return { books };
+const mapStateToProps = ({ books, loading, error }) => {
+  return { books, loading, error };
 };
 
-const mapDispatchToProps = {
-  booksLoaded
+const mapDispatchToProps = (dispatch, { bookstoreService }) => {
+  return {
+    fetchBooks: fetchBooks(bookstoreService, dispatch),
+  };
 };
 
 export default compose(
--- src/components/shop-header/shop-header.js
+++ src/components/shop-header/shop-header.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './shop-header.css';
 
 const ShopHeader = ({ numItems, total }) => {
   return (
     <header className="shop-header row">
-      <a className="logo text-dark" href="#">ReStore</a>
-      <a className="shopping-cart">
-        <i className="cart-icon fa fa-shopping-cart" />
-        {numItems} items (${total})
-      </a>
+      <Link to="/">
+        <div className="logo text-dark" href="#">ReStore</div>
+      </Link>
+      <Link to="/cart">
+        <div className="shopping-cart">
+          <i className="cart-icon fa fa-shopping-cart" />
+          {numItems} items (${total})
+        </div>
+        </Link>
     </header>
   );
 };
