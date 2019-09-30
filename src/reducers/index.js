cd6fa8debb3463d51a597ed8b8e258b396bb517b
vlad3k <vlad3k@bk.ru>
2019-09-30 16:30:09 +0500

    connect ShoppingCartTable redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,21 @@ const initialState = {
   books: [],
   loading: true,
   error: null,
+  cartItems: [
+    {
+      id: 1,
+      name: 'Book 1',
+      count: 3,
+      total: 150,
+    },
+    {
+      id: 2,
+      name: 'Book 2',
+      count: 2,
+      total: 700,
+    },
+  ],
+  orderTotal: 220,
 };
 
 const reducer = (state = initialState, action) => {
@@ -10,12 +25,14 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'FETCH_BOOKS_REQUEST':
       return {
+        ...state,
         books: [],
         loading: true,
         error: null,
       };
     case 'FETCH_BOOKS_SUCCESS':
       return {
+        ...state,
         books: action.payload,
         loading: false,
         error: null,
@@ -23,6 +40,7 @@ const reducer = (state = initialState, action) => {
     
     case 'FETCH_BOOKS_FAILURE':
       return {
+        ...state,
         books: [],
         loading: false,
         error: action.payload,
