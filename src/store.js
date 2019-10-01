9f4964d257aec9d5a8e223e9f633829578001eaf
vlad3k <vlad3k@bk.ru>
2019-10-01 19:55:20 +0500

    Middleware

--- src/store.js
+++ src/store.js
@@ -1,7 +1,25 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import reducer from './reducers';
 
-const store = createStore(reducer);
+const logMiddleware = ({ getState }) => (dispatch) => (action) => {
+  console.log(action.type, getState());
+  return dispatch(action);
+};
+
+const stringMiddleware = () => (dispatch) => (action) => {
+  if (typeof action === 'string') {
+    return dispatch({
+      type: action,
+    });
+  }
+
+  return dispatch(action);
+}
+
+const store = createStore(reducer, applyMiddleware(
+  stringMiddleware, logMiddleware));
+
+store.dispatch('HELLO_WORLD');
 
 export default store;
